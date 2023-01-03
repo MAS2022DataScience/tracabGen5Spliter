e562ddf6f99307d459691fb62e423544396ee576
Pascal Hubacher <pascal.hubacher@outlook.com>
2023-01-03 23:03:58 +0100

    modify to schema TracabGen5TF01

--- src/main/java/com/mas2022datascience/tracabgen5spliter/processor/KafkaStreamsRunnerDSL.java
+++ src/main/java/com/mas2022datascience/tracabgen5spliter/processor/KafkaStreamsRunnerDSL.java
@@ -1,8 +1,8 @@
 package com.mas2022datascience.tracabgen5spliter.processor;
 
-import com.mas2022datascience.avro.v1.Frame;
 import com.mas2022datascience.avro.v1.Object;
 import com.mas2022datascience.avro.v1.PlayerBall;
+import com.mas2022datascience.avro.v1.TracabGen5TF01;
 import io.confluent.kafka.streams.serdes.avro.SpecificAvroSerde;
 import java.time.Instant;
 import java.time.ZoneOffset;
@@ -32,27 +32,27 @@ public class KafkaStreamsRunnerDSL {
   @Value(value = "${topic.general-02.name}") private String topicOutTeam;
 
   @Bean
-  public KStream<String, Frame> kStream(StreamsBuilder kStreamBuilder) {
+  public KStream<String, TracabGen5TF01> kStream(StreamsBuilder kStreamBuilder) {
 
     // When you want to override serdes explicitly/selectively
     final Map<String, String> serdeConfig = Collections.singletonMap("schema.registry.url",
         schemaRegistry);
-    final Serde<Frame> frameSerde = new SpecificAvroSerde<>();
-    frameSerde.configure(serdeConfig, false); // `false` for record values
+    final Serde<TracabGen5TF01> tracabGen5TF01Serde = new SpecificAvroSerde<>();
+    tracabGen5TF01Serde.configure(serdeConfig, false); // `false` for record values
 
-    KStream<String, Frame> stream = kStreamBuilder.stream(topicIn,
-        Consumed.with(Serdes.String(), frameSerde));
+    KStream<String, TracabGen5TF01> stream = kStreamBuilder.stream(topicIn,
+        Consumed.with(Serdes.String(), tracabGen5TF01Serde));
 
     KStream<String, PlayerBall> playerStream = stream.flatMap(
       (key, value) -> {
         List<KeyValue<String, PlayerBall>> result = new LinkedList<>();
         for ( Object valueObject : value.getObjects() ) {
           result.add(KeyValue.pair(
-              value.getMatch().getId()+"-"+valueObject.getId(),
+              key+"-"+valueObject.getId(),
               PlayerBall
                   .newBuilder()
                   .setId(valueObject.getId())
-                  .setMatchId(key.split("-")[0])
+                  .setMatchId(key)
                   .setTs(Instant.ofEpochMilli(utcString2epocMs(value.getUtc())))
                   .setX(valueObject.getX())
                   .setY(valueObject.getY())
@@ -69,7 +69,7 @@ public class KafkaStreamsRunnerDSL {
     );
     playerStream.to(topicOutPlayer);
 
-    playerStream.filter((key, value) -> value.getId().equals("250101463")).to("temp");
+    //playerStream.filter((key, value) -> value.getId().equals("250101463")).to("temp");
 
     return stream;
 
@@ -77,11 +77,11 @@ public class KafkaStreamsRunnerDSL {
 
   /**
    * Converts the utc string of type "yyyy-MM-dd'T'HH:mm:ss.SSS" to epoc time in milliseconds.
-   * @param utcString of type String of format 'yyyy-MM-dd'T'HH:mm:ss.SSS'
+   * @param utcString of type String of format "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'"
    * @return epoc time in milliseconds
    */
   private static long utcString2epocMs(String utcString) {
-    DateTimeFormatter fmt = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSS")
+    DateTimeFormatter fmt = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'")
         .withZone(ZoneOffset.UTC);
 
     return Instant.from(fmt.parse(utcString)).toEpochMilli();
