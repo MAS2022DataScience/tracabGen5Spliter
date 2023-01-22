f96fdb601f93ab31858b0557d574522221a9fb0d
Pascal Hubacher <pascal.hubacher@outlook.com>
2023-01-22 22:42:46 +0100

    add "zone"

--- src/main/java/com/mas2022datascience/tracabgen5spliter/processor/KafkaStreamsRunnerDSL.java
+++ src/main/java/com/mas2022datascience/tracabgen5spliter/processor/KafkaStreamsRunnerDSL.java
@@ -5,9 +5,6 @@ import com.mas2022datascience.avro.v1.PlayerBall;
 import com.mas2022datascience.avro.v1.TracabGen5TF01;
 import com.mas2022datascience.util.Player;
 import io.confluent.kafka.streams.serdes.avro.SpecificAvroSerde;
-import java.time.Instant;
-import java.time.ZoneOffset;
-import java.time.format.DateTimeFormatter;
 import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
@@ -27,11 +24,16 @@ import org.springframework.kafka.annotation.EnableKafkaStreams;
 @EnableKafkaStreams
 public class KafkaStreamsRunnerDSL {
 
-  @Value(value = "${spring.kafka.properties.schema.registry.url}") private String schemaRegistry;
-  @Value(value = "${topic.tracab-01.name}") private String topicIn;
-  @Value(value = "${topic.general-01.name}") private String topicOutPlayerBall;
-  @Value(value = "${topic.general-02.name}") private String topicOutTeam;
-  @Value(value = "${topic.general-03.name}") private String topicOutPlayerBallCompact;
+  @Value(value = "${spring.kafka.properties.schema.registry.url}")
+  private String schemaRegistry;
+  @Value(value = "${topic.tracab-01.name}")
+  private String topicIn;
+  @Value(value = "${topic.general-01.name}")
+  private String topicOutPlayerBall;
+  @Value(value = "${topic.general-02.name}")
+  private String topicOutTeam;
+  @Value(value = "${topic.general-03.name}")
+  private String topicOutPlayerBallCompact;
 
   @Bean
   public KStream<String, TracabGen5TF01> kStream(StreamsBuilder kStreamBuilder) {
@@ -46,41 +48,22 @@ public class KafkaStreamsRunnerDSL {
         Consumed.with(Serdes.String(), tracabGen5TF01Serde));
 
     KStream<String, PlayerBall> playerStream = stream.flatMap(
-      (key, value) -> {
-        List<KeyValue<String, PlayerBall>> result = new LinkedList<>();
-        for ( Object valueObject : value.getObjects() ) {
-          if (valueObject.getType() == 0 || valueObject.getType() == 1
-              && valueObject.getPlayerId() != null) { // player
-            result.add(KeyValue.pair(
-                    key+"-"+valueObject.getPlayerId(),
-                    PlayerBall
-                        .newBuilder()
-                        .setTs(value.getTs())
-                        .setPlayerId(Player.getPlayerOrBallId(valueObject))
-                        .setTeamId(valueObject.getTeamId())
-                        .setMatchId(value.getMatchId())
-                        .setIsBallInPlay(value.getIsBallInPlay())
-                        .setBallPossession(value.getBallPossession())
-                        .setX(valueObject.getX())
-                        .setY(valueObject.getY())
-                        .setZ(valueObject.getZ())
-                        .setVelocity(valueObject.getVelocity())
-                        .setAccelleration(valueObject.getAccelleration())
-                        .setDistance(valueObject.getDistance())
-                        .build()
-                )
-            );
-          } else {
-            if (valueObject.getType() == 7) { // ball
+        (key, value) -> {
+          List<KeyValue<String, PlayerBall>> result = new LinkedList<>();
+          for (Object valueObject : value.getObjects()) {
+            if (valueObject.getType() == 0 || valueObject.getType() == 1
+                && valueObject.getPlayerId() != null) { // player
               result.add(KeyValue.pair(
-                      key+"-"+valueObject.getId(),
+                      key + "-" + valueObject.getPlayerId(),
                       PlayerBall
                           .newBuilder()
                           .setTs(value.getTs())
-                          .setPlayerId("0")
+                          .setPlayerId(Player.getPlayerOrBallId(valueObject))
+                          .setTeamId(valueObject.getTeamId())
                           .setMatchId(value.getMatchId())
                           .setIsBallInPlay(value.getIsBallInPlay())
                           .setBallPossession(value.getBallPossession())
+                          .setZone(valueObject.getZone())
                           .setX(valueObject.getX())
                           .setY(valueObject.getY())
                           .setZ(valueObject.getZ())
@@ -90,11 +73,32 @@ public class KafkaStreamsRunnerDSL {
                           .build()
                   )
               );
+            } else {
+              if (valueObject.getType() == 7) { // ball
+                result.add(KeyValue.pair(
+                        key + "-" + valueObject.getId(),
+                        PlayerBall
+                            .newBuilder()
+                            .setTs(value.getTs())
+                            .setPlayerId("0")
+                            .setMatchId(value.getMatchId())
+                            .setIsBallInPlay(value.getIsBallInPlay())
+                            .setBallPossession(value.getBallPossession())
+                            .setZone(valueObject.getZone())
+                            .setX(valueObject.getX())
+                            .setY(valueObject.getY())
+                            .setZ(valueObject.getZ())
+                            .setVelocity(valueObject.getVelocity())
+                            .setAccelleration(valueObject.getAccelleration())
+                            .setDistance(valueObject.getDistance())
+                            .build()
+                    )
+                );
+              }
             }
           }
+          return result;
         }
-        return result;
-      }
     );
     playerStream.to(topicOutPlayerBall);
 
@@ -105,18 +109,6 @@ public class KafkaStreamsRunnerDSL {
 
     return stream;
 
-  }
-
-  /**
-   * Converts the utc string of type "yyyy-MM-dd'T'HH:mm:ss.SSS" to epoc time in milliseconds.
-   * @param utcString of type String of format "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'"
-   * @return epoc time in milliseconds
-   */
-  private static long utcString2epocMs(String utcString) {
-    DateTimeFormatter fmt = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'")
-        .withZone(ZoneOffset.UTC);
-
-    return Instant.from(fmt.parse(utcString)).toEpochMilli();
   }
 }
 
