69e59ebcbabf0e7b61ce19fd46898d6ec99a865a
Pascal Hubacher <pascal.hubacher@outlook.com>
2023-03-19 15:11:54 +0100

    docker image ready

--- Dockerfile
+++ Dockerfile
@@ -3,8 +3,8 @@ FROM openjdk:20-jdk-slim-buster
 ENV DATAPLATFORM_IP=localhost
 
 WORKDIR /app
-COPY ./target/tracabGen5Spliter-0.0.1-SNAPSHOT.jar /app
+COPY ./target/tracabgen5spliter-0.0.1-SNAPSHOT.jar /app
 
 EXPOSE 8080
 
-CMD ["java", "-jar", "tracabGen5Spliter-0.0.1-SNAPSHOT.jar"]
+CMD ["java", "-jar", "tracabgen5spliter-0.0.1-SNAPSHOT.jar"]
--- dockerize.cmd
+++ dockerize.cmd
@@ -1,6 +1,8 @@
-docker build -t tracabGen5Spliter:0.0.1 -t tracabGen5Spliter:latest .
+REM docker build -t tracabgen5spliter:0.0.1 -t tracabgen5spliter:latest .
+docker rmi tracabgen5spliter:latest -f
+docker build -t tracabgen5spliter:latest .
 
 REM if it runs on localhost
-REM docker run -it -p 8080:8080 tracabGen5Spliter:latest
+REM docker run -it -p 8080:8080 tracabgen5spliter:latest
 REM if it runs on 192.168.1.100
-REM docker run --env DATAPLATFORM_IP=192.168.1.100 -it -p 8080:8080 tracabGen5Spliter:latest
+REM docker run --env DATAPLATFORM_IP=192.168.1.100 -it -p 8080:8080 tracabgen5spliter:latest
